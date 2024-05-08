3d02de09459e8d384a5943660fd35e6b0a064ab2
qiaqialuguo <502975825@qq.com>
2024-05-08 11:20:00 +0800

    日常

--- rag_classification/api_tools/tool_vehicle_issues.py
+++ rag_classification/api_tools/tool_vehicle_issues.py
@@ -14,23 +14,23 @@ def tool_wrapper_for_qwen_vehicle_issues():
             # 模型抽取校验
             if '' != value:
                 query_tmp[key] = value
-        if not query_tmp:
-            query_tmp['question'] = original_question
-            query_tmp['userId'] = user_id
+        # if not query_tmp:
+        query_tmp['question'] = original_question
+        query_tmp['userId'] = user_id
         # 如果用户说了车型信息，需要补充全
-        else:
-            if ('vehicle_brand_name' not in query_tmp or 'vehicle_series' not in query_tmp
-                    or 'vehicle_model_year' not in query_tmp):
-                missing_keys = [key for key in
-                                ['vehicle_brand_name', 'vehicle_series', 'vehicle_model_year'] if key not in query_tmp]
-                already_list = [(key, value) for key, value in already_known_user['used_car_valuation'].items()]
-                mapping_dict = {}
-                mapping_dict['vehicle_brand_name'] = '车辆品牌名称'
-                mapping_dict['vehicle_series'] = '车系'
-                mapping_dict['vehicle_model_year'] = '车辆年款'
-                missing_keys = [mapping_dict[item] if item in mapping_dict else item for item in missing_keys]
-                # already_list = [mapping_dict[item] if item in mapping_dict else item for item in already_list]
-                return f"需要继续询问用户{' 和 '.join(missing_keys)}", already_known_user
+        # else:
+        #     if ('vehicle_brand_name' not in query_tmp or 'vehicle_series' not in query_tmp
+        #             or 'vehicle_model_year' not in query_tmp):
+        #         missing_keys = [key for key in
+        #                         ['vehicle_brand_name', 'vehicle_series', 'vehicle_model_year'] if key not in query_tmp]
+        #         already_list = [(key, value) for key, value in already_known_user['used_car_valuation'].items()]
+        #         mapping_dict = {}
+        #         mapping_dict['vehicle_brand_name'] = '车辆品牌名称'
+        #         mapping_dict['vehicle_series'] = '车系'
+        #         mapping_dict['vehicle_model_year'] = '车辆年款'
+        #         missing_keys = [mapping_dict[item] if item in mapping_dict else item for item in missing_keys]
+        #         # already_list = [mapping_dict[item] if item in mapping_dict else item for item in already_list]
+        #         return f"需要继续询问用户{' 和 '.join(missing_keys)}", already_known_user
 
         query = query_tmp
         print(query)
