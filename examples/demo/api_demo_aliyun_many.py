240935b7f5e58fa980a7f4e2d4ea084b880a85e1
ubiai <502975825@qq.com>
2024-07-25 17:07:03 +0800

    日常

--- examples/demo/api_demo_aliyun_many.py
+++ examples/demo/api_demo_aliyun_many.py
@@ -1,6 +1,3 @@
 import uvicorn
-
-from rag_classification import rag_args_classification
-
 if __name__ == "__main__":
     uvicorn.run("api_demo_aliyun:app", host="0.0.0.0", port=10029, workers=10)
